8ff2152a15c95e621310ac15b0285f1b0ee8e26d
Your Name <you@example.com>
2020-07-01 21:07:41 +0200

    envoi mes fichiers esistants

---
+++ footer.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
+
+<footer>
+<p> © 2020 NEDRA - tous droits réservés </p>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<h1> <?php bloginfo('name'); ?> </h1>
+<main id="accueil">
+        
+    <?php while (have_posts()): the_post();?>
+        <article>
+        <a href="<?php the_permalink(); ?>">
+            <?php the_post_thumbnail('meduim'); ?>
+              <h2> <?php the_title(); ?> </h2>
+           </a>
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>">
+            Lire
+            </a>
+        </article>
+    <?php endwhile; ?>
+</main>
+<?php get_footer(); ?>
